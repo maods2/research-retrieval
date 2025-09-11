3dd9b8c5944afb012e8254723e21459c96c1fd9c
davidolima <davidlima@ufba.br>
2025-09-11 18:10:15 -0300

    wip:  Use CPathFM Lib EmbeddingCache as a dataset option

--- src/factories/dataset_factory.py
+++ src/factories/dataset_factory.py
@@ -1,24 +1,50 @@
+from typing import Optional, Callable
+
+import torch
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+
+import pathology_foundation_models as pfm
+
 from dataloaders.dataset import StandardImageDataset
 from dataloaders.dataset_contrastive import ContrastiveDataset
 from dataloaders.dataset_fewshot import FewShotFolderDataset
 from dataloaders.dataset_triplet import TripletDataset
-from torch.utils.data import DataLoader
+from src.utils.auth_utils import get_hf_token
 
+def get_embedding_dataset(config: dict, transform_train, transform_test):
+    new_cfg = config.copy()
+    new_cfg['data']['dataset_type'] = 'StandardImageDataset'
+    train_img_set, test_img_set = get_dataset(new_cfg, transform_train, transform_test)
+    del new_cfg
 
-def get_dataloader(config, transform_train, transforms_test):
-    """
-    Factory function to get data loaders for train and test sets.
+    train_embed_dataset = pfm.dataset.EmbeddingCache.init_from_image_dataset(
+        image_dataset = train_img_set,
+        model = pfm.models.load_foundation_model(
+            model_type = config['model']['model_name'],
+            device = 'cuda' if torch.cuda.is_available() else 'cpu', 
+            token = get_hf_token()
+        ),
+        batch_size = config['data']['batch_size_train'],
+        num_workers = config['data']['num_workers']
+    )
 
-    Args:
-        config (dict): Configuration dict containing information on data directories,
-                             transformations, and other parameters.
-        transform (A.Compose): Composed Albumentations transformation pipeline.
+    test_embed_dataset = pfm.dataset.EmbeddingCache.init_from_image_dataset(
+        image_dataset = test_img_set,
+        model = pfm.models.load_foundation_model(
+            model_type = config['model']['model_name'],
+            device = 'cuda' if torch.cuda.is_available() else 'cpu', 
+            token = get_hf_token()
+        ),
+        batch_size = config['data']['batch_size_test'],
+        num_workers = config['data']['num_workers']
+    )
 
-    Returns:
-        train_loader (DataLoader): PyTorch DataLoader for the training set
-        test_loader (DataLoader): PyTorch DataLoader for the test set
-    """
+    return train_embed_dataset, test_embed_dataset
+
+def get_dataset(config, transform_train: Optional[Callable] = None, transform_test: Optional[Callable] = None):
     data_config = config['data']  # Extract data config from the main config
+
     # Get transformations based on config
     dataset_name = data_config.get(
         'dataset_type', 'StandardImageDataset'
@@ -33,8 +59,11 @@ def get_dataloader(config, transform_train, transforms_test):
         dataset_class = FewShotFolderDataset
     elif dataset_name == 'ContrastiveDataset':
         dataset_class = ContrastiveDataset
+    elif dataset_name == 'EmbeddingDataset':
+        return get_embedding_dataset(config, transform_train, transform_test)
     else:
         raise ValueError(f'Dataset {dataset_name} is not supported.')
+
     # Create dataset instances for training and evaluation
     train_dataset = dataset_class(
         root_dir=data_config['train_dir'],  # Directory for training data
@@ -45,16 +74,39 @@ def get_dataloader(config, transform_train, transforms_test):
 
     test_dataset = dataset_class(
         root_dir=data_config['test_dir'],  # Directory for evaluation data
-        transform=transforms_test,  # Transformations to apply
+        transform=transform_test,  # Transformations to apply
         class_mapping=data_config['class_mapping'],  # Custom class mappings
         config=config,  # Additional config for dataset
+    )
+
+    return train_dataset, test_dataset
+
+def get_dataloader(config, transform_train: Optional[Callable] = None, transform_test: Optional[Callable] = None):
+    """
+    Factory function to get data loaders for train and test sets.
+
+    Args:
+        config (dict): Configuration dict containing information on data directories,
+                             transformations, and other parameters.
+        transform (A.Compose): Composed Albumentations transformation pipeline.
+
+    Returns:
+        train_loader (DataLoader): PyTorch DataLoader for the training set
+        test_loader (DataLoader): PyTorch DataLoader for the test set
+    """
+    data_config = config['data']  # Extract data config from the main config
+
+    train_dataset, test_dataset = get_dataset(
+        config=config,
+        transform_train=transform_train,
+        transform_test=transform_test
     )
 
     # Create DataLoader instances for both datasets
     train_loader = DataLoader(
         train_dataset,
         batch_size=data_config['batch_size_train'],  # Define the batch size
-        shuffle=data_config.get('suffle_train', True),  # Shuffle for training
+        shuffle=data_config.get('shuffle_train', True),  # Shuffle for training
         num_workers=data_config[
             'num_workers'
         ],  # Number of workers for data loading
@@ -69,7 +121,7 @@ def get_dataloader(config, transform_train, transforms_test):
         test_dataset,
         batch_size=batch_size_eval,  # Define the batch size
         shuffle=data_config.get(
-            'suffle_test', False
+            'shuffle_test', False
         ),  # No need to shuffle test set
         num_workers=data_config[
             'num_workers'
