bcf3b1e274f69a4899d180b6bd87682c43284533
Matheus Augusto <matheusalgusto_@hotmail.com>
2025-10-20 07:50:30 -0300

    Add support for SupCon and Supervised Hashing trainers in training pipeline

--- src/factories/train_factory.py
+++ src/factories/train_factory.py
@@ -10,6 +10,16 @@ def get_train_function(config):
 
         return FewShotTrainer(config)
 
+    elif config['training']['pipeline'] == 'supcon_trainer':
+        from pipelines.training_pipes.supcon_trainer import SupConTrainer
+
+        return SupConTrainer(config)
+
+    elif config['training']['pipeline'] == 'supervised_hashing_trainer':
+        from pipelines.training_pipes.supervised_hashing_trainer import SupervisedHashingTrainer
+
+        return SupervisedHashingTrainer(config)
+
     else:
         raise ValueError(
             f'Training pipeline {config["training"]["pipeline"]} is not supported'
